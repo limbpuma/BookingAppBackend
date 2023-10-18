998a7a1361df5168ea9b3ec62d7277508211a2a4
Limber <limbpuma_de@hotmail.com>
2023-10-18 12:15:57 +0200

    changing cross to all x

--- src/main/java/restaurant/booking/controller/BookingController.java
+++ src/main/java/restaurant/booking/controller/BookingController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 //localhost:8081/restaurant-app
 @RequestMapping("restaurant-app")
-@CrossOrigin(origins = "web-production-f34d5.up.railway.app")
+@CrossOrigin(origins = "*")
 
 public class BookingController {
 
